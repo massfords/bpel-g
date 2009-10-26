95369c22d1182cc1788b6b4b0132dcf3b0458947
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-10-26 22:20:44 +0000

    removed null and console loggers since commons-logging is better

--- bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/logging/IAeLogWrapper.java
+++ bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/logging/IAeLogWrapper.java
@@ -9,76 +9,12 @@
 /////////////////////////////////////////////////////////////////////////////
 package org.activebpel.rt.bpel.server.logging;
 
-import org.activebpel.rt.bpel.server.AeMessages;
 
 /**
  *  Wrapper around app server logging.
  */
 public interface IAeLogWrapper
 {
-   /**
-    * Simple console logging. 
-    */
-   public static final IAeLogWrapper CONSOLE_LOG = new IAeLogWrapper()
-   {
-      /**
-       * Debug messages.
-       * @param aMessage
-       */
-      public void logDebug( String aMessage )
-      {
-         System.out.println( AeMessages.getString("IAeLogWrapper.0") + aMessage ); //$NON-NLS-1$
-      }
-
-      /**
-       * Info messages.
-       * @param aMessage
-       */
-      public void logInfo( String aMessage )
-      {
-         System.out.println( AeMessages.getString("IAeLogWrapper.1") + aMessage ); //$NON-NLS-1$
-      }
-   
-      /**
-       * Error messages.
-       * @param aMessage
-       * @param aProblem
-       */
-      public void logError( String aMessage, Throwable aProblem )
-      {
-         System.out.println( AeMessages.getString("IAeLogWrapper.2") + aMessage ); //$NON-NLS-1$
-         if( aProblem != null )
-         {
-            aProblem.printStackTrace();
-      	}
-      }
-   };
-   
-   /** No-op IAeLogWrapper impl. */
-   public static IAeLogWrapper NULL_LOG = new IAeLogWrapper()
-   {
-      /**
-       * @see org.activebpel.rt.bpel.server.logging.IAeLogWrapper#logDebug(java.lang.String)
-       */
-      public void logDebug(String aMessage)
-      {
-      }
-
-      /**
-       * @see org.activebpel.rt.bpel.server.logging.IAeLogWrapper#logError(java.lang.String, java.lang.Throwable)
-       */
-      public void logError(String aMessage, Throwable aProblem)
-      {
-      }
-
-      /**
-       * @see org.activebpel.rt.bpel.server.logging.IAeLogWrapper#logInfo(java.lang.String)
-       */
-      public void logInfo(String aMessage)
-      {
-      }
-   };
-
    /**
     * Debug messages.
     * @param aMessage
