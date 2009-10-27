d0d10f4170e91ed44aafd89128703d9a882b0386
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-10-27 20:26:46 +0000

    javadoc

--- trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/BgDeploymentLogger.java
+++ trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/BgDeploymentLogger.java
@@ -4,6 +4,11 @@ import org.activebpel.rt.bpel.server.logging.AeDeploymentLog;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * Commons logging version of the deployment log.
+ * 
+ * @author mford
+ */
 public class BgDeploymentLogger extends AeDeploymentLog {
     
     private static final Log sLog = LogFactory.getLog(BgDeploymentLogger.class);
