ce1dcef6cdb4b28dbf20393cbc18ecf8184e17dc
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-10-30 21:27:30 +0000

    routes messages into the engine (actually works!)

--- bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/exchange/BgMessageExchangeProcessor.java
+++ bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/exchange/BgMessageExchangeProcessor.java
@@ -1,19 +1,24 @@
 package bpelg.jbi.exchange;
 
+import javax.jbi.messaging.ExchangeStatus;
+import javax.jbi.messaging.Fault;
 import javax.jbi.messaging.InOnly;
 import javax.jbi.messaging.InOut;
 import javax.jbi.messaging.MessageExchange;
 import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
 import javax.jbi.servicedesc.ServiceEndpoint;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
 
 import org.activebpel.rt.bpel.server.engine.AeEngineFactory;
 import org.activebpel.wsio.IAeWebServiceResponse;
 import org.activebpel.wsio.receive.AeMessageContext;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import bpelg.jbi.BgBpelService;
 import bpelg.jbi.BgContext;
@@ -27,8 +32,12 @@ public class BgMessageExchangeProcessor implements IBgMessageExchangeProcessor {
             // FIXME call queueInvokeData / queueInvokeFault
         } else if (aMex.getRole().equals(javax.jbi.messaging.MessageExchange.Role.PROVIDER)) {
             
-            // FIXME call queueReceiveData
-            // use mex.getEndpoint() as a key to get the processName + partnerLink we're targeting
+            if (aMex.getStatus() != ExchangeStatus.ACTIVE) {
+                // We can forget about the exchange.
+                return;
+            }
+
+            Exception error = null;
             
             try {
                 ServiceEndpoint serviceEndpoint = aMex.getEndpoint();
@@ -37,12 +46,35 @@ public class BgMessageExchangeProcessor implements IBgMessageExchangeProcessor {
                 AeMessageContext context = new AeMessageContext();
                 context.setProcessName(bpelService.getProcessName());
                 context.setPartnerLink(bpelService.getPartnerLinkDefKey().getPartnerLinkName());
-                IAeWebServiceResponse response = AeEngineFactory.getEngine().queueReceiveData(context, data);
                 
-                // FIXME handle reply with fault or data
-                // FIXME mark exchange as done.
+                // no durable replies for the moment
+                IAeWebServiceResponse response = AeEngineFactory.getEngine().queueReceiveData(context, data);
+                if (isTwoWay(aMex)) {
+                    InOut exchange = (InOut) aMex;
+                    DOMSource responseData = new DOMSource((Node) response.getMessageData().getMessageData().values().iterator().next());
+                    if (response.isFaultResponse()) {
+                        Fault fault = exchange.createFault();
+                        fault.setContent(responseData);
+                    } else {
+                        NormalizedMessage responseMessage = exchange.createMessage();
+                        responseMessage.setContent(responseData);
+                        exchange.setOutMessage(responseMessage);
+                    }
+                    BgContext.getInstance().getComponentContext().getDeliveryChannel().send(aMex);
+                } else {
+                    // nothing to do with one-way
+                }
             } catch (Exception e) {
-                // FIXME handle exception
+                error = e;
+            }
+            finally {
+                if (error != null) {
+                    aMex.setError(error);
+                    aMex.setStatus(ExchangeStatus.ERROR);
+                } else if (aMex.getStatus() == ExchangeStatus.ACTIVE)
+                    aMex.setStatus(ExchangeStatus.DONE);
+                
+                BgContext.getInstance().getComponentContext().getDeliveryChannel().send(aMex);
             }
         } else {
             // unexpected role, fault
@@ -51,7 +83,7 @@ public class BgMessageExchangeProcessor implements IBgMessageExchangeProcessor {
     
     protected Document getData(MessageExchange aMex) throws Exception {
         Source source = null;
-        if (aMex.getPattern().equals(BgMessageExchangePattern.IN_ONLY)) {
+        if (isOneWay(aMex)) {
             InOnly inOnly = (InOnly) aMex;
             source = inOnly.getInMessage().getContent();
         } else {
@@ -60,7 +92,15 @@ public class BgMessageExchangeProcessor implements IBgMessageExchangeProcessor {
         }
         return toDocument(source);
     }
+
+    protected boolean isOneWay(MessageExchange aMex) {
+        return aMex.getPattern().equals(BgMessageExchangePattern.IN_ONLY);
+    }
     
+    protected boolean isTwoWay(MessageExchange aMex) {
+        return aMex.getPattern().equals(BgMessageExchangePattern.IN_OUT);
+    }
+
     protected Document toDocument(Source aSource) throws Exception {
         TransformerFactory tf = TransformerFactory.newInstance();
         Transformer t = tf.newTransformer();
