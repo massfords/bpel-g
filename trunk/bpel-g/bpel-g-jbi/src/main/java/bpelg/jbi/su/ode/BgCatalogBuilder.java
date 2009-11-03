6dc178d6c39733ac50fae0be169220aaf0ddc59a
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-11-03 20:20:06 +0000

    more progress on getting ode style deployments work - very close now

--- trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/ode/BgCatalogBuilder.java
+++ trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/ode/BgCatalogBuilder.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import org.activebpel.rt.IAeConstants;
 import org.activebpel.rt.util.AeXmlUtil;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 
 /**
@@ -17,16 +18,43 @@ import org.w3c.dom.Document;
 public class BgCatalogBuilder {
     private File mServiceUnitRoot;
     private Collection<BgCatalogTuple> mCollection = new ArrayList();
-    private String mPhysicalPathPrefix;
+    private String mLogicalPathPrefix;
+    private Document mCatalog;
     
     public BgCatalogBuilder(File aRoot) {
         assert aRoot.isDirectory();
         mServiceUnitRoot = aRoot;
-        mPhysicalPathPrefix = "project://" + mServiceUnitRoot.getName() + "/";
+        mLogicalPathPrefix = "project:/" + mServiceUnitRoot.getName() + "/";
     }
     
     public void build() throws Exception {
         addFiles(mServiceUnitRoot, "");
+        buildCatalog();
+    }
+    
+    protected void buildCatalog() throws Exception {
+        Document doc = AeXmlUtil.newDocument();
+        String catalogNS = "http://schemas.active-endpoints.com/catalog/2006/07/catalog.xsd";
+        Element catalog = AeXmlUtil.addElementNS(doc, catalogNS, "catalog", null);
+        catalog.setAttributeNS(IAeConstants.W3C_XMLNS, "xmlns", catalogNS);
+        for(BgCatalogTuple tuple : mCollection) {
+            Element entry = null;
+            if (tuple.isWsdl()) {
+                entry = AeXmlUtil.addElementNS(catalog, catalogNS, "wsdlEntry");
+            } else if (tuple.isXsd()) {
+                entry = AeXmlUtil.addElementNS(catalog, catalogNS, "schemaEntry");
+            } else {
+                entry = AeXmlUtil.addElementNS(catalog, catalogNS, "otherEntry");
+                entry.setAttribute("type", tuple.type);
+            }
+            entry.setAttribute("location", tuple.logicalLocation);
+            entry.setAttribute("classpath", tuple.physicalLocation);
+        }
+        mCatalog = doc;
+    }
+    
+    protected Document getCatalog() {
+        return mCatalog;
     }
     
     protected void addFiles(File aCurrentDir, String aPath) throws Exception {
@@ -41,12 +69,12 @@ public class BgCatalogBuilder {
                 String name = file.getName().toLowerCase();
                 if (name.endsWith(".wsdl")) {
                     String namespace = getTargetNamespace(file);
-                    item = new BgCatalogTuple(path, mPhysicalPathPrefix + path, namespace, IAeConstants.WSDL_NAMESPACE);
+                    item = new BgCatalogTuple(mLogicalPathPrefix + path, path, namespace, IAeConstants.WSDL_NAMESPACE);
                 } else if (name.endsWith(".xsd")) {
                     String namespace = getTargetNamespace(file);
-                    item = new BgCatalogTuple(path, mPhysicalPathPrefix + path, namespace, IAeConstants.W3C_XML_SCHEMA);
+                    item = new BgCatalogTuple(mLogicalPathPrefix + path, path, namespace, IAeConstants.W3C_XML_SCHEMA);
                 } else if (name.endsWith(".xsl")) {
-                    item = new BgCatalogTuple(path, mPhysicalPathPrefix + path, null, IAeConstants.XSL_NAMESPACE);
+                    item = new BgCatalogTuple(mLogicalPathPrefix + path, path, null, IAeConstants.XSL_NAMESPACE);
                 }
                 
                 if (item !=null)
--- trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/ode/BgPddBuilder.java
+++ trunk/bpel-g/bpel-g-jbi/src/main/java/bpelg/jbi/su/ode/BgPddBuilder.java
@@ -2,6 +2,7 @@ package bpelg.jbi.su.ode;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -10,24 +11,90 @@ import java.util.Map;
 import javax.xml.namespace.QName;
 
 import org.activebpel.rt.AeException;
+import org.activebpel.rt.IAeConstants;
+import org.activebpel.rt.util.AeUtil;
 import org.activebpel.rt.util.AeXPathUtil;
 import org.activebpel.rt.util.AeXmlUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import bpelg.jbi.su.ode.BgPddInfo.BgPlink;
+
 public class BgPddBuilder {
+    
+    private static final String PDD = "http://schemas.active-endpoints.com/pdd/2006/08/pdd.xsd";
+    private static final String WSA = "http://www.w3.org/2005/08/addressing";
     
     private static final Map NAMESPACES = Collections.singletonMap("ode", "http://www.apache.org/ode/schemas/dd/2007/03");
     
     private File mServiceUnitRoot;
     
     private Map<QName, BgPddInfo> mDeployments = new HashMap();
+    private Map<String,BgPddInfo> mFileToPddMap = new HashMap();
     
     public BgPddBuilder(File aServiceUnitRoot) {
         assert aServiceUnitRoot.isDirectory();
         assert new File(aServiceUnitRoot, "deploy.xml").isFile();
         
         mServiceUnitRoot = aServiceUnitRoot;
+    }
+    
+    public Collection<String> getPddNames() {
+        return mFileToPddMap.keySet();
+    }
+    
+    public Document getPdd(String aName, Collection<BgCatalogTuple> aCatalog) {
+        BgPddInfo info = mFileToPddMap.get(aName);
+        Document doc = AeXmlUtil.newDocument();
+        Element pdd = AeXmlUtil.addElementNS(doc, PDD, "pdd:process");
+        pdd.setAttributeNS(IAeConstants.W3C_XMLNS, "xmlns:pdd", PDD);
+        pdd.setAttributeNS(IAeConstants.W3C_XMLNS, "xmlns:bpelns", info.getProcessName().getNamespaceURI());
+        pdd.setAttribute("name", "bpelns:" + info.getProcessName().getLocalPart());
+        pdd.setAttribute("platform", "opensource");
+        pdd.setAttribute("location", aName + ".pdd");
+        
+        Element plinks = AeXmlUtil.addElementNS(pdd, PDD, "pdd:partnerLinks");
+
+        for(BgPlink plink : info.getBgPlinks()) {
+            Element plinkEl = AeXmlUtil.addElementNS(plinks, PDD, "pdd:partnerLink");
+            plinkEl.setAttribute("name", plink.name);
+            if (plink.hasMyRole()) {
+                Element myRole = AeXmlUtil.addElementNS(plinkEl, PDD, "pdd:myRole");
+                myRole.setAttribute("allowedRoles", "");
+                myRole.setAttribute("binding", "MSG");
+                String encodedService = AeXmlUtil.encodeQName(plink.myService, myRole, "mysvc");
+                // space delimited value of Service QName + endpoint
+                myRole.setAttribute("service", encodedService + " " + plink.myEndpoint);
+            }
+            if (plink.hasPartnerRole()) {
+                Element partnerRole = AeXmlUtil.addElementNS(plinkEl, PDD, "pdd:partnerRole");
+                partnerRole.setAttribute("endpointReference", "static");
+                partnerRole.setAttribute("invokeHandler", "default:Service");
+                Element epr = AeXmlUtil.addElementNS(partnerRole, WSA, "wsa:EndpointReference");
+                epr.setAttributeNS(IAeConstants.W3C_XMLNS, "xmlns:wsa", WSA);
+                AeXmlUtil.addElementNS(epr, WSA, "wsa:Address", "None");
+                Element metadata = AeXmlUtil.addElementNS(epr, WSA, "wsa:Metadata");
+                String encodedService = AeXmlUtil.encodeQName(plink.partnerService, metadata, "psvc");
+                Element serviceName = AeXmlUtil.addElementNS(metadata, WSA, "wsa:ServiceName", encodedService);
+                serviceName.setAttribute("PortName", plink.partnerEndpoint);
+            }
+        }
+        
+        Element refs = AeXmlUtil.addElementNS(pdd, PDD, "pdd:references");
+        for(BgCatalogTuple catalogEntry : aCatalog) {
+            Element entry = null;
+            if (catalogEntry.isWsdl()) {
+                entry = AeXmlUtil.addElementNS(refs, PDD, "pdd:wsdl");
+            } else if (catalogEntry.isXsd()) {
+                entry = AeXmlUtil.addElementNS(refs, PDD, "pdd:schema");
+            } else {
+                entry = AeXmlUtil.addElementNS(refs, PDD, "pdd:other");
+            }
+            
+            entry.setAttribute("location", catalogEntry.logicalLocation);
+            entry.setAttribute("namespace", AeUtil.getSafeString(catalogEntry.namespace));
+        }
+        return doc;
     }
     
     public void build() throws Exception {
@@ -42,7 +109,8 @@ public class BgPddBuilder {
             String location = processNameToLoc.get(processName);
             
             BgPddInfo pddInfo = new BgPddInfo(processName, location);
-            mDeployments.put(processName, pddInfo);
+            mDeployments.put(pddInfo.getProcessName(), pddInfo);
+            mFileToPddMap.put(pddInfo.getLocation(), pddInfo);
             
             // add provides
             List<Element> providesService = AeXPathUtil.selectNodes(process, "ode:provide/ode:service", NAMESPACES);
@@ -68,13 +136,7 @@ public class BgPddBuilder {
         
         Map<QName,String> locationMap = new HashMap();
         
-        File[] files = mServiceUnitRoot.listFiles(new FileFilter() {
-
-            @Override
-            public boolean accept(File aFile) {
-                return aFile.getName().endsWith(".bpel");
-            }
-        });
+        File[] files = getBpelFiles();
         
         for(File file : files) {
             Document doc = AeXmlUtil.toDoc(file, null);
@@ -83,6 +145,17 @@ public class BgPddBuilder {
             locationMap.put(processName, location);
         }
         return locationMap;
+    }
+
+    private File[] getBpelFiles() {
+        File[] files = mServiceUnitRoot.listFiles(new FileFilter() {
+
+            @Override
+            public boolean accept(File aFile) {
+                return aFile.getName().endsWith(".bpel");
+            }
+        });
+        return files;
     }
     
     protected Map<QName,BgPddInfo> getDeployments() {
