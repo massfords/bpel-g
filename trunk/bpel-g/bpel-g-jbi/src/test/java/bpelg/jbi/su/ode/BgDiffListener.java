2c1544246cd308e17ffc6067c19c378866e51835
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-11-04 03:25:01 +0000

    added ignore paths

--- trunk/bpel-g/bpel-g-jbi/src/test/java/bpelg/jbi/su/ode/BgDiffListener.java
+++ trunk/bpel-g/bpel-g-jbi/src/test/java/bpelg/jbi/su/ode/BgDiffListener.java
@@ -1,5 +1,9 @@
 package bpelg.jbi.su.ode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.custommonkey.xmlunit.Difference;
 import org.custommonkey.xmlunit.DifferenceConstants;
 import org.custommonkey.xmlunit.DifferenceListener;
@@ -7,15 +11,26 @@ import org.w3c.dom.Node;
 
 public class BgDiffListener implements DifferenceListener {
     
+    protected Set<String> mIgnorePaths = new HashSet();
+    
     public int differenceFound(Difference aDifference)
     {
-        if (aDifference.getId() == DifferenceConstants.NAMESPACE_PREFIX_ID)
+        if (aDifference.getId() == DifferenceConstants.NAMESPACE_PREFIX_ID || isIgnored(aDifference.getTestNodeDetail().getXpathLocation()))
             return DifferenceListener.RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
         return DifferenceListener.RETURN_ACCEPT_DIFFERENCE;
     }
 
     public void skippedComparison(Node aControl, Node aTest)
     {
+    }
+    
+    public void setIgnorePaths(String...aPaths) {
+        if (aPaths !=null)
+            mIgnorePaths.addAll(Arrays.asList(aPaths));
+    }
+    
+    protected boolean isIgnored(String aXPath) {
+        return mIgnorePaths.contains(aXPath);
     }
 
 }
