655d60146d7d9ba410739143a7dd07fe92795432
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-11-12 02:35:22 +0000

    more management methods

--- trunk/bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/admin/jmx/IAeEngineManagementMXBean.java
+++ trunk/bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/admin/jmx/IAeEngineManagementMXBean.java
@@ -2,6 +2,24 @@ package org.activebpel.rt.bpel.server.admin.jmx;
 
 import java.util.List;
 
+import org.activebpel.rt.bpel.impl.list.AeProcessInstanceDetail;
+import org.activebpel.rt.bpel.server.admin.AeProcessDeploymentDetail;
+
 public interface IAeEngineManagementMXBean {
+
+    /**
+     * Gets the details for all of the deployed services.
+     */
     public List<AeServiceDeploymentBean> getDeployedServices();
+
+    /**
+     * Gets the details for all of the deployed processes
+     */
+    public List<AeProcessDeploymentDetail> getDeployedProcesses();
+
+    /**
+     * Gets the details for a single process id
+     * @param aId
+     */
+    public AeProcessInstanceDetail getProcessDetail(long aId);
 }
