29f984464649ce78a488a33f01957fd06b01cf8d
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-11-14 22:11:30 +0000

    serializable tweaks for jmx admin console

--- bpel-g/org.activebpel.rt.bpel/src/main/java/org/activebpel/rt/bpel/def/activity/support/AeLiteralDef.java
+++ bpel-g/org.activebpel.rt.bpel/src/main/java/org/activebpel/rt/bpel/def/activity/support/AeLiteralDef.java
@@ -10,14 +10,20 @@
 
 package org.activebpel.rt.bpel.def.activity.support;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.activebpel.rt.AeException;
 import org.activebpel.rt.bpel.def.AeBaseDef;
+import org.activebpel.rt.bpel.def.IAeBPELConstants;
 import org.activebpel.rt.bpel.def.visitors.IAeDefVisitor;
 import org.activebpel.rt.util.AeXmlUtil;
+import org.activebpel.rt.xml.AeXMLParserBase;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Models the new literal bpel construct wrapper introduced in WS-BPEL 2.0.
@@ -25,7 +31,7 @@ import org.w3c.dom.Node;
 public class AeLiteralDef extends AeBaseDef
 {
    /** The list of child nodes in the literal. */
-   private List mChildNodes = new ArrayList();
+   private transient List<Node> mChildNodes = new ArrayList();
 
    /**
     * Default c'tor.
@@ -40,7 +46,7 @@ public class AeLiteralDef extends AeBaseDef
     * extra child nodes that we may have read in. We'll produce an error message for multiple children during validation.
     * @return Returns the childNodes.
     */
-   public List getChildNodes()
+   public List<Node> getChildNodes()
    {
       return mChildNodes;
    }
@@ -69,4 +75,31 @@ public class AeLiteralDef extends AeBaseDef
    {
       aVisitor.visit(this);
    }
+
+   private void writeObject(java.io.ObjectOutputStream out)
+   throws IOException {
+       Document doc = AeXmlUtil.newDocument();
+       Element e = AeXmlUtil.addElementNS(doc, IAeBPELConstants.WSBPEL_2_0_NAMESPACE_URI, "literal");
+       for (Node node : mChildNodes ) {
+           Node n = doc.importNode(node, true);
+           e.appendChild(n);
+       }
+       String xml = AeXMLParserBase.documentToString(doc);
+       out.writeUTF(xml);
+   }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        String xml = in.readUTF();
+        try {
+            Document doc = new AeXMLParserBase(true,false).loadDocumentFromString(xml, null);
+            mChildNodes = new ArrayList();
+            NodeList childNodes = doc.getDocumentElement().getChildNodes();
+            for(int i=0; i<childNodes.getLength(); i++) {
+                addChildNode(childNodes.item(i));
+            }
+        } catch (AeException e) {
+            throw new IOException(e);
+        }
+    }
 }
