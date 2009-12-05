fd2586c8b2cf9543a4efd2c9c376d8d1f8735dbe
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2009-12-05 12:27:37 +0000

    extracted the jmx connection info context params

--- bpel-g/org.activebpel.rt.bpeladmin.jar/src/main/java/org/activebpel/rt/bpeladmin/war/AeContextListener.java
+++ bpel-g/org.activebpel.rt.bpeladmin.jar/src/main/java/org/activebpel/rt/bpeladmin/war/AeContextListener.java
@@ -1,17 +1,29 @@
 package org.activebpel.rt.bpeladmin.war;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class AeContextListener implements ServletContextListener{
 
     @Override
-    public void contextDestroyed(ServletContextEvent aArg0) {
+    public void contextDestroyed(ServletContextEvent aContextEvent) {
         AeEngineManagementFactory.close();
     }
 
     @Override
-    public void contextInitialized(ServletContextEvent aArg0) {
+    public void contextInitialized(ServletContextEvent aContextEvent) {
+        ServletContext servletContext = aContextEvent.getServletContext();
+        String serviceURL = servletContext.getInitParameter("jmx.serviceURL");
+        String objectName = servletContext.getInitParameter("jmx.objectName");
+        String user = servletContext.getInitParameter("jmx.user");
+        String password = servletContext.getInitParameter("jmx.password");
+        
+        try {
+            AeEngineManagementFactory.initBean(serviceURL, objectName, user, password);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
