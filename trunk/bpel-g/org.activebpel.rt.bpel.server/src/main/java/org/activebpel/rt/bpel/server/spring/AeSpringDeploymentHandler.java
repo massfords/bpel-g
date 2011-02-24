6bf45940b4635ae21ce86f160cc3985087ce3970
mark@bpelpeople.com <mark@bpelpeople.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2011-02-24 21:47:09 +0000

    - handles the deployment and undeployment of spring contexts - removed dupe method for classloader on context

--- trunk/bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/spring/AeSpringDeploymentHandler.java
+++ trunk/bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/spring/AeSpringDeploymentHandler.java
@@ -9,24 +9,35 @@ import org.activebpel.rt.bpel.server.logging.IAeDeploymentLogger;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.xml.sax.InputSource;
+import org.springframework.core.io.UrlResource;
 
+/**
+ * Handles the deployment of a spring context within a deployment container.
+ * For now, the context MUST be located at META-INF/applicationContext.xml
+ * 
+ * @author mford
+ */
 public class AeSpringDeploymentHandler implements IAeDeploymentHandler {
 
+	/** reference to the manager that holds onto the context */
 	private AeSpringManager mSpringManager;
 	
 	@Override
 	public void deploy(IAeDeploymentContainer aContainer,
 			IAeDeploymentLogger aLogger) throws AeException {
+		// check to see if there's a context
 		URL context = aContainer.getResourceURL("META-INF/applicationContext.xml");
+		// if we don't find one, there's nothing to do
 		if (context != null) {
-			URL location = aContainer.getDeploymentLocation();
+			// create a GenericApplicationContext. Using a the generic in order to be able
+			// to set the resource loader in place so it uses the classloader bound to this
+			// directory.
 			GenericApplicationContext ac = new GenericApplicationContext();
 			ac.setResourceLoader(new DefaultResourceLoader(aContainer.getResourceClassLoader()));
 			XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ac);
-			xmlReader.loadBeanDefinitions(new InputSource(location.toExternalForm()));
+			xmlReader.loadBeanDefinitions(new UrlResource(context));
 			ac.refresh();
-			getSpringManager().add(location.toExternalForm(), ac);
+			getSpringManager().add(context.toExternalForm(), ac);
 		}
 	}
 
@@ -34,8 +45,7 @@ public class AeSpringDeploymentHandler implements IAeDeploymentHandler {
 	public void undeploy(IAeDeploymentContainer aContainer) throws AeException {
 		URL context = aContainer.getResourceURL("META-INF/applicationContext.xml");
 		if (context != null) {
-			URL location = aContainer.getDeploymentLocation();
-			GenericApplicationContext ac = getSpringManager().remove(location.toExternalForm());
+			GenericApplicationContext ac = getSpringManager().remove(context.toExternalForm());
 			if (ac != null) {
 				ac.close();
 			}
