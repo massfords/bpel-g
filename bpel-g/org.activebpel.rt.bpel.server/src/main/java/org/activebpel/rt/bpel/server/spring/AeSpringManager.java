cba8b077333caf416e9ba5d0b4570be2819ca8b6
mark@massfords.com <mark@massfords.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2011-07-14 04:23:15 +0000

    - map process qnames to their contexts to allow access to CamelContext from the deployment - support single part doc style response fro camel invoke handler - add integration test for camel invoke handler changes

--- bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/spring/AeSpringManager.java
+++ bpel-g/org.activebpel.rt.bpel.server/src/main/java/org/activebpel/rt/bpel/server/spring/AeSpringManager.java
@@ -1,9 +1,14 @@
 package org.activebpel.rt.bpel.server.spring;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.xml.namespace.QName;
 
 import org.activebpel.rt.bpel.impl.AeManagerAdapter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
@@ -15,7 +20,11 @@ import org.springframework.context.support.GenericApplicationContext;
 public class AeSpringManager extends AeManagerAdapter {
 
     /** map of contexts */
-    private Map<String,GenericApplicationContext> mContextMap = new HashMap<String, GenericApplicationContext>();
+    private Map<String,GenericApplicationContext> mContextMap = new ConcurrentHashMap<String, GenericApplicationContext>();
+    /** map of process qnames to their contexts */
+    private Map<QName,GenericApplicationContext> mQNameContextMap = new ConcurrentHashMap<QName, GenericApplicationContext>();
+    /** map of keys to sets */
+    private Map<String,Set<QName>> mNameMap = new ConcurrentHashMap<String, Set<QName>>();
 
     /* start all of the contexts upon manager start
      * @see org.activebpel.rt.bpel.impl.AeManagerAdapter#start()
@@ -43,8 +52,15 @@ public class AeSpringManager extends AeManagerAdapter {
      * @param aKey
      * @param aContext
      */
-    public void add(String aKey, GenericApplicationContext aContext) {
+    public void add(Set<QName> aProcessSet, String aKey, GenericApplicationContext aContext) {
+        // record the key to context
         mContextMap.put(aKey, aContext);
+        // record the key to the process set
+        mNameMap.put(aKey, aProcessSet);
+        // record all of the individual process qnames
+        for(QName name : aProcessSet) {
+            mQNameContextMap.put(name, aContext);
+        }
         aContext.start();
     }
     
@@ -53,10 +69,30 @@ public class AeSpringManager extends AeManagerAdapter {
      * @param aKey
      */
     public GenericApplicationContext remove(String aKey) {
+        // remove the context
     	GenericApplicationContext context = mContextMap.remove(aKey);
+    	// remove the process names
+        Set<QName> names = mNameMap.get(aKey);
+        if (names != null) {
+            for(QName name : names) {
+                mQNameContextMap.remove(name);
+            }
+        }
+        // remove the name set
+        mNameMap.remove(aKey);
         if (context != null) {
             context.stop();
         }
         return context;
+    }
+    
+    public <T> T getBean(QName aProcessName, Class<T> aClass) throws BeansException {
+        ApplicationContext ac = mContextMap.get(aProcessName);
+        return ac.getBean(aClass);
+    }
+
+    public <T> T getBean(QName aProcessName, Class<T> aClass, String aId) throws BeansException {
+        ApplicationContext ac = mContextMap.get(aProcessName);
+        return ac.getBean(aId, aClass);
     }
 }
