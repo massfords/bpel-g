3e4c2efd426a755f83d02458285744da8ad496ea
mark@massfords.com <mark@massfords.com@99488b1a-bb55-11de-8784-f7ad4773dbab>
2012-04-07 02:42:47 +0000

    - updated the correlation test (wasn't including the deploy.xml file) - added missing namespace to the xquery in the correlation bpel - fixed the service name in the correlation bpel

--- trunk/bpel-g/bpel-g.war/src/test/java/org/activebpel/services/jaxws/CorrelationIntegrationTest.java
+++ trunk/bpel-g/bpel-g.war/src/test/java/org/activebpel/services/jaxws/CorrelationIntegrationTest.java
@@ -7,10 +7,7 @@ import org.junit.*;
 import org.w3c.dom.Document;
 
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.StringReader;
+import java.io.*;
 import java.util.Collections;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
@@ -19,26 +16,18 @@ import java.util.zip.ZipEntry;
  * @author markford
  *         Date: 4/6/12
  */
-@Ignore
 public class CorrelationIntegrationTest extends Assert {
     AeProcessFixture pfix = new AeProcessFixture();
 
     @Before
     public void setUp() throws Exception {
+
         // zip the bpel and such
         JarOutputStream jout = new JarOutputStream(new FileOutputStream("target/correlation.zip"));
-        jout.putNextEntry(new ZipEntry("correlation.bpel"));
-        jout.write(IOUtils.toByteArray(new FileInputStream("src/test/resources/correlation/correlation.bpel")));
-        jout.closeEntry();
 
-        jout.putNextEntry(new ZipEntry("correlation.wsdl"));
-        jout.write(IOUtils.toByteArray(new FileInputStream("src/test/resources/correlation/correlation.wsdl")));
-        jout.closeEntry();
-
-        jout.putNextEntry(new ZipEntry("skip.validation"));
-        jout.write("skip".getBytes());
-        jout.closeEntry();
-
+        addToZip(jout, new File("src/test/resources/correlation/correlation.bpel"),
+                        new File("src/test/resources/correlation/correlation.wsdl"),
+                        new File("src/test/resources/correlation/deploy.xml"));
         jout.flush();
         jout.close();
 
@@ -49,11 +38,12 @@ public class CorrelationIntegrationTest extends Assert {
     @Test
     public void test() throws Exception {
 
+        // invoke the service and get our quote back
         Document quote = pfix.invoke(new File("src/test/resources/correlation/requestForQuote.xml"),
                 "http://localhost:8080/bpel-g/services/quoteService");
-
         assertNotNull(quote);
 
+        // send another message saying that we agree to the quote
         String quoteId = AeXPathUtil.selectText(quote, "//q:quoteId",
                 Collections.singletonMap("q", "http://www.example.org/correlation/"));
 
@@ -72,4 +62,13 @@ public class CorrelationIntegrationTest extends Assert {
     public void tearDown() throws Exception {
         pfix.getDeployer().undeploy(new UndeploymentRequest().withDeploymentContainerId("correlation.zip"));
     }
+
+    private void addToZip(JarOutputStream jout, File...files) throws IOException {
+        for(File file : files) {
+            jout.putNextEntry(new ZipEntry(file.getName()));
+            jout.write(IOUtils.toByteArray(new FileInputStream(file)));
+            jout.closeEntry();
+        }
+    }
+
 }
